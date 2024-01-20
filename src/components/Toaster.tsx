158305084bf58cea38f7b013f598e1b3a3b08bb5
sheriff Oladimeji <dimejiademola5@gmail.com>
2024-01-20 13:29:40 +0100

    feat: add toast notificaions

---
+++ src/components/Toaster.tsx
@@ -0,0 +1,29 @@
+import { toast, Bounce } from "react-toastify";
+
+ export const successMessage = (message: string) => {
+  toast.success(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Bounce,
+  });
+};
+
+ export const errorMessage = (message: string) => {
+   toast.error(message, {
+     position: "top-right",
+     autoClose: 5000,
+     hideProgressBar: false,
+     closeOnClick: true,
+     pauseOnHover: true,
+     draggable: true,
+     progress: undefined,
+     theme: "dark",
+     transition: Bounce,
+   });
+ };
