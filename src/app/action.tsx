dd7e25753da8428ddb6cb4b7d844134772d62e70
sheriff Oladimeji <dimejiademola5@gmail.com>
2024-01-20 14:30:59 +0100

    fix : update action.tsx

--- src/app/action.tsx
+++ src/app/action.tsx
@@ -1,5 +1,6 @@
 "use server";
 import axios from "axios";
+import { ToastContainer } from "react-toastify";
 const LIMIT = 8;
 
 export async function fetchAnimes(page: number) {
@@ -7,9 +8,30 @@ export async function fetchAnimes(page: number) {
     const response = await axios.get(
       `https://shikimori.one/api/animes?page=${page}&limit=${LIMIT}&order=popularity`
     );
-      const data = response.data;
-      return data
+    const data = response.data;
+    return data;
   } catch (error) {
-    console.error("Error fetching data: ", error);
+    console.error("Error fetching animes: ", error);
+  }
+}
+export async function fetchAnime(id: number) {
+  try {
+    const anime = await axios.get(`https://shikimori.one/api/animes/:${id}`);
+    const video = await axios.get(
+      `https://shikimori.one/api/animes/:anime_id/videos`
+    );
+    const data = { anime: anime.data, video: video.data };
+    return data;
+  } catch (error) {
+    console.error("Error fetching anime: ", error);
+  }
+}
+export async function searchAnime(page: number) {
+  try {
+    const response = await axios.get(`https://shikimori.one/api/animes/search`);
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error("Error fetching animes: ", error);
   }
 }
